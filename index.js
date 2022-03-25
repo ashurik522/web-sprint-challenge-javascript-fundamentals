0402a785e9e1b398519756f92bee53e8f979e2b1
ashurik522 <ashurik522@gmail.com>
2022-03-25 06:50:02 -0400

    task2

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function myFunction() {
 //myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
+//The nested function can access anything within the function it is nested in and on the global scope. If const internal was in stead declared in the 
+//nested function, it would only be accesible there. 
 
 
 
@@ -30,11 +31,14 @@ function myFunction() {
 💡 NOTE: you may use a for loop for this function if you wish 
 */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+  let count = 0;
+  for (let i = 0; i<= number; i++){
+    count += i;
   }
- 
+  return count
+} 
+ console.log(summation(4))
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
